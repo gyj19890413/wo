9ff90c9d67e78d5852eabfdb3029ed7ae5c02cee
Your Name <754571968@qq.com>
2017-10-20 09:57:46 +0800

    sadfsadfg

--- js/js.js
+++
@@ -1,9 +0,0 @@
-window.onload =function(){
-
-	window.addEventListener('resize',setRize,false);
-	function setRize (){
-		// 用户改变浏览器大小    自适应
-		document.documentElement.style.fontSize=document.documentElement.clientWidth/6.4+'px';
-	};
-	setRize();
-}
