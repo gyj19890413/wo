14ec119d9588c3b01bebf376423c03b961e09ec1
Your Name <754571968@qq.com>
2017-11-09 16:14:09 +0800

    注释

--- wode/application/index/model/Sms.php
+++ wode/application/index/model/Sms.php
@@ -1,13 +1,5 @@
 <?php
-// +----------------------------------------------------------------------
-// | 海豚PHP框架 [ DolphinPHP ]
-// +----------------------------------------------------------------------
-// | 版权所有 2016~2017 河源市卓锐科技有限公司 [ http://www.zrthink.com ]
-// +----------------------------------------------------------------------
-// | 官方网站: http://dolphinphp.com
-// +----------------------------------------------------------------------
-// | 开源协议 ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
+
 
 namespace app\index\model;
 
@@ -44,16 +36,20 @@ protected $autoWriteTimestamp = true;
 		return $data;
     }
     /***
-     * 登录
-     * 
+     * 添加验证码到数据库
+     * `id` int(11) NOT NULL AUTO_INCREMENT,
+     * `phone` char(11) NOT NULL COMMENT '手机号',
+	  `type` varchar(4) NOT NULL DEFAULT '1' COMMENT '类型 1登录验证码 ',
+	  `code` varchar(10) NOT NULL COMMENT '手机验证码',
+	  `sendstate` tinyint(2) NOT NULL DEFAULT '0',
+	  `checkstate` tinyint(2) NOT NULL DEFAULT '0' COMMENT '验证状态 1已验证 0未验证',
+	  `date` varchar(30) NOT NULL DEFAULT '使用时间' COMMENT '日期',
+	  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	  `ip` varchar(50) DEFAULT NULL COMMENT 'Ip',
      */
-    public static function addSms($phone){
+    public static function addSms(){
     	
-//  	$data=[
-//  		'phone'=>$phone,
-//  		'type'=>1,
-//  		'code'=>
-//  	];
+
     	
     }
 }
